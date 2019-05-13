f7855ec38f39160091668601a9f9abcfd69e6314
Paul Holmes <paulholmes1993@gmail.com>
2019-05-13 00:55:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,23 +1,20 @@
-speakers = [
-  "Edsger",
-  "Ada",
-  "Charles",
-  "Alan",
-  "Grace",
-  "Linus",
-  "Matz"
-]
+def create_badge(*names)
+  badge_array = []
+  names.each { |name| badge_array << "Hello, my name is #{name}." }
 
-def create_badge(names)
-  names.map { |name| name = "Hello, my name is #{name}." }
+  badge_array
 end
 
-def get_rooms(names)
-  room = 0
-  names.map do |name|
-    room += 1
-    "Hello #{name}! You'll be assigned to room #{room}!"
+def get_rooms(list)
+  rooms = [1, 2, 3, 4, 5, 6, 7]
+  room_assignments = []
+  speaker_counter = 0
+  rooms.each do |room|
+    room_assignments << "Hello, #{list[speaker_counter].split(' ')[-1].sub('.', '')}! You'll be assigned to room #{room}!"
+    speaker_counter += 1
   end
+
+  room_assignments
 end
 
 def print_stuff(badge_list, room_assignments)
@@ -25,10 +22,10 @@ def print_stuff(badge_list, room_assignments)
   puts room_assignments
 end
 
-def generate(speakers)
-  badges = create_badge(speakers)
-  rooms = get_rooms(speakers)
+def generate
+  badges = create_badge("Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz")
+  rooms = get_rooms(badges)
   print_stuff(badges, rooms)
 end
 
-generate(speakers)
+generate
