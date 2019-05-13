0f769ed2d98bb879f9c969c4c69198aaeaaffa56
Paul Holmes <paulholmes1993@gmail.com>
2019-05-13 00:54:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def create_badge(names)
+  names.map { |name| name = "Hello, my name is #{name}." }
+end
+
+def get_rooms(names)
+  room = 0
+  names.map do |name|
+    room += 1
+    "Hello #{name}! You'll be assigned to room #{room}!"
+  end
+end
+
+def print_stuff(badge_list, room_assignments)
+  puts badge_list
+  puts room_assignments
+end
+
+def generate(speakers)
+  badges = create_badge(speakers)
+  rooms = get_rooms(speakers)
+  print_stuff(badges, rooms)
+end
+
+generate(speakers)
